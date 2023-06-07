5b8f401f0cf03531b004a5e497a14a7c8730df52
Adophilus <uchenna19of@gmail.com>
2023-06-07 23:38:47 +0100

    began working on solution 1 of day 1 in zig

--- day-1/cpp/src/solution_1.cpp
+++ day-1/cpp/src/solution_1.cpp
@@ -4,7 +4,20 @@
 
 int main() {
   const std::string input_file = "input.txt";
+  std::string line;
   std::fstream file(input_file);
+
+  if (!file.is_open()) {
+    print_error("Failed to open file!");
+    return 1;
+  }
+
+  line = file.get();
+  cout << line << std::endl;
+
+  // while (getline(file, line)) {
+  //   std::cout << line << std::endl;
+  // }
 
   file.close();
 }
